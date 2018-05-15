4099c919d61173b2ebffd9f214ada0e19792d6ae
abenjamin1313 <abenjamin1313@gmail.com>
2018-05-14 20:31:15 -0400

    Working prototype.

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+// Component Imports
+import NavBar from './components/navbar/NavBar';
+import Search from './components/search/Search';
+
+class App extends Component {
+
+  
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <NavBar />
+          <Search />
+        </div>
+      </MuiThemeProvider>
+    );
+      
+  }
+}
+
+export default App;
